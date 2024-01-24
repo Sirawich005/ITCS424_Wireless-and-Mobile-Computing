bf965bb2d39040a64b6b0359da07df2e74aea9e4
Sirawich005 <144013287+Sirawich005@users.noreply.github.com>
2024-01-24 12:13:42 +0700

    Update main.dart

--- Week3/main.dart
+++ Week3/main.dart
@@ -26,7 +26,7 @@ class Content extends StatelessWidget {
       children: [
         Counter('64880005'),
         Counter('Sirawich Anantapong'),
-        Counter('Mr.books'),
+        Counter('Mr.book'),
         Counter('6488005, 6488017, 6488071, 6488113'),
       ],
     );
